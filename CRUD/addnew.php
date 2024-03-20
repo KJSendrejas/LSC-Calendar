5021468547f1b6573d0c28f012ab8e1bf90e643c
Kent Jude Sendrejas <kj.sendrejas@gmail.com>
2024-03-20 10:05:02 +0800

    CRUD Backup

---
+++ CRUD/addnew.php
@@ -0,0 +1,191 @@
+<link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+<script src="//code.jquery.com/jquery-3.6.0.min.js"></script>
+<script src="//code.jquery.com/ui/1.12.1/jquery-ui.js"></script>
+
+<?php
+// Include config file
+require_once "config.php";
+ 
+// Define variables and initialize with empty values
+$id  = $date = $name = $broll = $spbet = $winlose = $shares = "";
+$id_err  = $date_err = $name_err = $start_broll_err = $unit_psize_err = $win_lose_err = $shares_err = "";
+ 
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+    
+     // Validate id
+     $input_id = trim($_POST["id"]);
+     if(empty($input_id)){
+        $id_err = "Please enter the account id.";     
+    } else{
+        $id = $input_id;
+    }
+
+    // Validate date
+    $input_date = trim($_POST["date"]);
+    if(empty($input_date)){
+    $date_err = "Please enter date.";     
+    } else{
+    $date = $input_date;
+    }
+
+    // Validate name
+    $input_name = trim($_POST["name"]);
+    if(empty($input_name)){
+    $name_err = "Please enter a name.";
+    } else{
+        $name = $input_name;
+    }
+    
+    // Validate bankroll
+    $input_broll = trim($_POST["broll"]);
+    if(empty($input_broll)){
+        $start_broll_err = "Please enter the starting bankroll.";     
+    } elseif(!ctype_digit($input_broll)){
+        $start_broll_err = "Please enter a valid input.";
+    } else{
+        $broll = $input_broll;
+    }
+
+    // Validate unit size per bet
+    $input_spbet = trim($_POST["spbet"]);
+    if(empty($input_spbet)){
+        $unit_psize_err = "Please enter the unit size amount.";     
+    } elseif(!ctype_digit($input_spbet)){
+        $unit_psize_err = "Please enter a valid input.";
+    } else{
+        $spbet = $input_spbet;
+    }
+
+     // Validate win/lose
+     $input_winlose = trim($_POST["winlose"]);
+     if(empty($input_winlose)){
+         $win_lose_err = "Please enter the win or lose amount.";     
+     } elseif(!ctype_digit($input_winlose)){
+         $win_lose_err = "Please enter a valid input.";
+     } else{
+         $winlose = $input_winlose;
+     }
+
+     // Validate shares
+     $input_shares = trim($_POST["shares"]);
+     if(empty($input_shares)){
+         $shares_err = "Please enter the share amount.";     
+     } elseif(!ctype_digit($input_shares)){
+         $shares_err = "Please enter a valid input.";
+     } else{
+         $shares = $input_shares;
+     }
+    
+    // Check input errors before inserting in database
+    if(empty($id_err) && empty($date_err) && empty($name_err) && empty($start_broll_err) && empty($unit_psize_err) && empty($winlose_err) && empty($shares_err)){
+        // Prepare an insert statement
+        $sql = "INSERT INTO members (id , date , name, start_broll, unit_pbet, win_lose, share ) VALUES (?, ?, ?, ?, ?, ?, ?)";
+         
+        if($stmt = mysqli_prepare($conn, $sql)){
+            // Bind variables to the prepared statement as parameters
+            mysqli_stmt_bind_param($stmt, "sssssss", $param_id, $param_date, $param_name, $param_broll, $param_spbet, $param_winlose, $param_shares);
+            
+            // Set parameters
+            $param_id = $id;
+            $param_date = $date;
+            $param_name = $name;
+            $param_broll = $broll;
+            $param_spbet = $spbet;
+            $param_winlose = $winlose;
+            $param_shares = $shares;
+            
+            // Attempt to execute the prepared statement
+            if(mysqli_stmt_execute($stmt)){
+                // Records created successfully. Redirect to landing page
+                header("location: calendar2.php");
+                exit();
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+         
+        // Close statement
+        mysqli_stmt_close($stmt);
+    }
+    
+    // Close connection
+    mysqli_close($conn);
+}
+?>
+ 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Create Record</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .wrapper{
+            width: 600px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="mt-5">Add New</h2>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+
+                        <div class="form-group">
+                            <label>ID</label>
+                            <input type="text" name="id" class="form-control <?php echo (!empty($id_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $id; ?>">
+                            <span class="invalid-feedback"><?php echo $id_err;?></span>
+                        </div>
+                        <script>
+                        $(document).ready(function(){
+                            $('input[name="date"]').datepicker({
+                                dateFormat: 'yy-mm-dd' // Adjust the date format as needed
+                            });
+                        });
+                        </script>
+                        <div class="form-group">
+                        <label>Date</label>
+                        <input type="date" name="date" class="form-control <?php echo (!empty($date_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $date; ?>">
+                        <span class="invalid-feedback"><?php echo $date_err;?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Name</label>
+                            <input type="text" name="name" class="form-control <?php echo (!empty($name_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $name; ?>">
+                            <span class="invalid-feedback"><?php echo $name_err;?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Starting Bankroll</label>
+                            <input type="text" name="broll" class="form-control <?php echo (!empty($start_broll_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $broll; ?>">
+                            <span class="invalid-feedback"><?php echo $start_broll_err;?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Unit Size per Bet</label>
+                            <input type="text" name="spbet" class="form-control <?php echo (!empty($unit_psize_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $spbet; ?>">
+                            <span class="invalid-feedback"><?php echo $unit_psize_err;?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Win/Lose</label>
+                            <input type="text" name="winlose" class="form-control <?php echo (!empty($win_lose_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $winlose; ?>">
+                            <span class="invalid-feedback"><?php echo $win_lose_err;?></span>
+                        </div>
+                        <div class="form-group">
+                            <label>Shares</label>
+                            <input type="text" name="shares" class="form-control <?php echo (!empty($shares_err)) ? 'is-invalid' : ''; ?>" value="<?php echo $shares; ?>">
+                            <span class="invalid-feedback"><?php echo $shares_err;?></span>
+                        </div>
+                        
+
+                        <input type="submit" class="btn btn-success fa fa-plus" value="Add New">
+                        <a href="calendar2.php" class="btn btn-secondary ml-2">Cancel</a>
+
+                    </form>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
---
+++ CRUD/read.php
@@ -0,0 +1,110 @@
+<?php
+// Check existence of id parameter before processing further
+if(isset($_GET["id"]) && !empty(trim($_GET["id"]))){
+    // Include config file
+    require_once "config.php";
+    
+    // Prepare a select statement
+    $sql = "SELECT * FROM members WHERE id = ?";
+    
+    if($stmt = mysqli_prepare($conn, $sql)){
+        // Bind variables to the prepared statement as parameters
+        mysqli_stmt_bind_param($stmt, "i", $param_id);
+        
+        // Set parameters
+        $param_id = trim($_GET["id"]);
+        
+        // Attempt to execute the prepared statement
+        if(mysqli_stmt_execute($stmt)){
+            $result = mysqli_stmt_get_result($stmt);
+    
+            if(mysqli_num_rows($result) == 1){
+                /* Fetch result row as an associative array. Since the result set
+                contains only one row, we don't need to use while loop */
+                $row = mysqli_fetch_array($result, MYSQLI_ASSOC);
+                
+                // Retrieve individual field value
+                $id = $row ["id"];
+                $date  = $row["date"];
+                $name = $row["name"];
+                $broll = $row["start_broll"];
+                $spbet = $row["unit_pbet"];
+                $winlose = $row["win_lose"];
+                $shares = $row["share"];
+            } else{
+                //URL doesn't contain valid id parameter. Redirect to error page
+                header("location: error.php");
+                exit();
+            }
+            
+        } else{
+            echo "Oops! Something went wrong. Please try again later.";
+        }
+    }
+     
+    // Close statement
+    mysqli_stmt_close($stmt);
+    
+    // Close connection
+    mysqli_close($conn);
+} else{
+    // URL doesn't contain id parameter. Redirect to error page
+    header("location: error.php");
+    exit();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>View Record</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <style>
+        .wrapper{
+            width: 600px;
+            margin: 0 auto;
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h1 class="mt-5 mb-3">View Record</h1>
+                    <div class="form-group">
+                        <label>ID</label>
+                        <p><b><?php echo $row["id"]; ?></b></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Date</label>
+                        <p><b><?php echo $row["date"]; ?></b></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Name</label>
+                        <p><b><?php echo $row["name"]; ?></b></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Starting Bankroll</label>
+                        <p><b><?php echo $row["start_broll"]; ?></b></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Unit Size</label>
+                        <p><b><?php echo $row["unit_pbet"]; ?></b></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Win/Lose</label>
+                        <p><b><?php echo $row["win_lose"]; ?></b></p>
+                    </div>
+                    <div class="form-group">
+                        <label>Shares</label>
+                        <p><b><?php echo $row["share"]; ?></b></p>
+                    </div>
+                    <p><a href="calendar2.php" class="btn btn-primary">Back</a></p>
+                </div>
+            </div>        
+        </div>
+    </div>
+</body>
+</html>
